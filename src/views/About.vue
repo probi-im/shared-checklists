05fc024e6b5be1e415ba9fc66dbe33c82f99f8ec
Bastien <probicieux44@gmail.com>
2021-02-07 18:49:27 +0100

    added header to about and setting views

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
-    <h1>This is the about view (to be completed)</h1>
+    <Header :title="'About'" :leadingIcon="false" />
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Header from "@/components/Header.vue";
+
+export default defineComponent({
+  name: "About",
+  components: {
+    Header,
+  },
+});
+</script>
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="settings">
-    <h1>This is the settings view (to be completed)</h1>
+    <Header :title="'Settings'" :leadingIcon="false" />
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Header from "@/components/Header.vue";
+
+export default defineComponent({
+  name: "Settings",
+  components: {
+    Header,
+  },
+});
+</script>
